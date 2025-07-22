14d4a3ac66a8f58c84d60b0e17733923432e7627
JHkashell <juon600@gmail.com>
2025-07-23 04:44:38 +0900

    Delete pages directory

--- pages/category/[category].tsx
+++
@@ -1,9 +0,0 @@
-const CategoryPage = () => {
-  return (
-    <div className="p-8 text-center">
-      <h1 className="text-2xl font-bold">카테고리별 뉴스 페이지 (준비 중)</h1>
-    </div>
-  );
-};
-
-export default CategoryPage;
